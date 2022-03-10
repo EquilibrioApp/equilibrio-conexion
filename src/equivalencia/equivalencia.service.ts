15f99ef78c11d9097a7a61777443cc8dd1fc7095
EquilibrioApp <equilibrio.veri@gmail.com>
2022-03-09 19:17:12 -0600

    Avance

--- src/equivalencia/equivalencia.service.ts
+++ src/equivalencia/equivalencia.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { resolve } from 'path/win32';
 import { Repository } from 'typeorm';
-import * as xlsm from "xlsx";
+import * as xlsm from "ts-xlsx";
 import {WorkBook, WorkSheet} from 'xlsx';
 import { EquivalenciaEntity } from './equivalencia.entity';
 
@@ -15,18 +15,11 @@ export class EquivalenciaService {
     ){}
 
     convertFile(equivalencia : Partial<EquivalenciaEntity>)  {
-        try {
-            
-            
-        } catch (error) {
-            
-        }
-
-        // const excel = xlsm.readFile('./HojaDeEquivalencia');
-        // const nombreHoja = excel.Sheets["Hoja"];
-        // const datos = xlsm.utils.sheet_to_json(nombreHoja[0]);
-        // // const datos = await XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[3]]);
-        // console.log(datos);
-        // return datos;
+        const excel = xlsm.readFile('./HojaDeEquivalencia');
+        const nombreHoja = excel.Sheets["Hoja"];
+        const datos = xlsm.utils.sheet_to_json(nombreHoja[0]);
+        // const datos = await XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[3]]);
+        console.log(datos);
+        return datos;
     }
 }
